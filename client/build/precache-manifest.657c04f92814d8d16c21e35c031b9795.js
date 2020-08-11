0d089bef31895ff6939c6abc14a7c7de60d21306
allen <theallenma@gmail.com>
2020-08-10 23:53:36 -0700

    Commit build folder

---
+++ client/build/precache-manifest.657c04f92814d8d16c21e35c031b9795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef768178036e3eba09987bd495201934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d8dc36e9c13e46874f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6d1618543aa65099fca",
+    "url": "/static/js/2.47d4b597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47d4b597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8dc36e9c13e46874f8",
+    "url": "/static/js/main.fea5c783.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
